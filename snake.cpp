c1c714e6534ee383dc44a9ca2d1c2e55e52ef4bf
Chiri Francisco <franchiri00@gmail.com>
2023-02-16 18:32:57 -0300

    Creacion de tablero

--- snake.cpp
+++ snake.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define ARRIBA 72
+#define ABAJO 80
+#define IZQUIERDA 75
+#define DERECHA 77
+#define ESQ 27
+
+void gotoxy(int x, int y){
+	
+	HANDLE hcon;
+	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;
+	dwPos.X = x;
+	dwPos.Y = y;
+	SetConsoleCursorPosition(hcon, dwPos);
+	
+}
+	
+void tablero(){
+	
+	for(int i=2; i<78; i++){
+		
+		gotoxy(i , 3); printf ("%c", 205);
+		gotoxy(i , 23); printf ("%c", 205);
+		
+	}
+	for(int i=4; i<23; i++){
+		
+		gotoxy(2 , i); printf ("%c", 186);
+		gotoxy(77 , i); printf ("%c", 186);
+		
+	}
+	gotoxy(2, 3); printf ("%c", 201);
+	gotoxy(77, 3); printf ("%c", 187);
+	gotoxy(77, 23); printf ("%c", 188);
+	gotoxy(2, 23); printf ("%c", 200);
+	
+}
+
 using namespace std;
-int edad;
+
 int main(int argc, char *argv[]) {
+	
+	tablero();
+	system("pause>null");
 	
 	return 0;
 }
